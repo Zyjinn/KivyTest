473004cdf7acc9f62b5049b074aae47182199c19
Zygen <Xsaber64@Gmail.com>
2020-08-25 01:15:57 -0500

    Created base template for new signin.py kivy file

--- signin.py
+++ signin.py
@@ -1,3 +1,14 @@
 # Import
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+
+class SigninWindow(BoxLayout):
+    pass
+
+class SigninApp(App):
+    def build(self):
+        return SigninWindow()
+
+if __name__ == "__main__":
+    # Create an instance of an app
+    SigninApp().run()
